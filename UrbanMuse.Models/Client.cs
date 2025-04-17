4405b58bebb676bb8fba7943bdf936c71dfc04ae
admin <udalovbg7@gmail.com>
2025-04-18 01:22:13 +0400

    добавил модели

---
+++ UrbanMuse.Models/Client.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibrary1;
+
+public class Client
+{
+    [Key] public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public DateTime BirthDate { get; set; }
+}
