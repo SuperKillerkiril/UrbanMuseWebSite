104fece00361d5e1000b3a46796dcb59c01f005b
admin <udalovbg7@gmail.com>
2025-04-19 18:04:06 +0400

    Страница /store и кастомный layout

--- UrbanMuse.Models/Order.cs
+++ UrbanMuse.Models/Order.cs
@@ -5,5 +5,15 @@ namespace UrbanMuse.Models;
 
 public class Order
 {
+    public Order(string comment)
+    {
+        Comment = comment;
+    }
+
     [Key] public int Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public DateTime ShippedDate { get; set; }
+    public Client? Client { get; set; }
+    public Product? Product { get; set; }
+    public string? Comment { get; set; }
 }
