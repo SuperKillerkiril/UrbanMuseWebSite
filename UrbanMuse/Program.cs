e0e5df3cf1ecd3b3d571f2847f8c4b4751e37b1d
admin <udalovbg7@gmail.com>
2025-08-09 17:31:12 +0400

    Renamed directories, moved and renamed file. Updated Auth.razor, Registred.razor.

--- UrbanMuse/Program.cs
+++ UrbanMuse/Program.cs
@@ -1,9 +1,11 @@
 using System.Net.Mime;
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using UrbanMuse.DataBase;
 using UrbanMuse.Models;
+using UrbanMuse.Services;
 using UrbanMuseWeb.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,15 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddDbContext<ModelContext>();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<AuthService>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/access-denied";
+    });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -35,6 +46,8 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
+app.UseAuthentication();
+app.UseAuthorization();
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<ModelContext>();
 context.Database.Migrate();
